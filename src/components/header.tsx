d778433b51472a39eaae52d5cedb07032e09ce7e
Lorenzo <lorenzo.acetii@gmail.com>
2023-10-30 19:42:23 -0300

    Change site title

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,9 @@ export default function Header() {
   return (
     <header className="fixed left-0 top-0 z-50 w-screen">
       <div className="flex items-center justify-center border-b border-slate-200 bg-white px-11 py-4">
-        <h1 className="text-2xl font-bold">DF-Analyzer Interface Generator</h1>
+        <h1 className="text-2xl font-bold">
+          Script Instrumentation for DfAnalyzer
+        </h1>
       </div>
     </header>
   )
